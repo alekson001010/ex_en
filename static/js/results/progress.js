08d1f76c97873915deaaab066f1f924d887233a5
fufic123 <semenovmark07@gmail.com>
2023-05-25 09:06:32 +0200

    color

--- static/js/results/progress.js
+++ static/js/results/progress.js
@@ -9,98 +9,97 @@ const success = document.getElementById('success')
 const failure = document.getElementById('failure')
 
 if (userGoal === 'increase') {
-    if (progress >= 2) {
-        img2.classList.add('active')
-    }
-    if (progress >= 5) {
-        img5.classList.add('active')
-    }
-    if (progress >= 7) {
-        img7.classList.add('active')
-    }
-    if (progress >= 10) {
-        img10.classList.add('active')
-    }
-
+  if (progress >= 2) {
+    img2.classList.add('active')
+  }
+  if (progress >= 5) {
+    img5.classList.add('active')
+  }
+  if (progress >= 7) {
+    img7.classList.add('active')
+  }
+  if (progress >= 10) {
+    img10.classList.add('active')
+  }
 } else {
-    if (progress <= -2) {
-        console.log('active 2')
-        img2.classList.add('active')
-    }
-    if (progress <= -5) {
-        img5.classList.add('active')
-    }
-    if (progress <= -7) {
-        img7.classList.add('active')
-    }
-    if (progress <= -10) {
-        img10.classList.add('active')
-    }
+  if (progress <= -2) {
+    console.log('active 2')
+    img2.classList.add('active')
+  }
+  if (progress <= -5) {
+    img5.classList.add('active')
+  }
+  if (progress <= -7) {
+    img7.classList.add('active')
+  }
+  if (progress <= -10) {
+    img10.classList.add('active')
+  }
 }
 if (userGoal === 'increase') {
-    if (progress > 0) {
-        let msg = document.getElementById('msgIncrease')
-        if (userGoal === 'increase') {
-            msg.style.color = 'rgb(0, 255, 0)'
-        } else {
-            msg.style.color = 'rgb(255, 0, 0)'
-        }
-        successDisplay(msg);
+  if (progress > 0) {
+    let msg = document.getElementById('msgIncrease')
+    if (userGoal === 'increase') {
+      msg.style.color = 'rgb(0, 0, 0)'
     } else {
-        if (progress < 0) {
-            let msg = document.getElementById('msgDecrease')
-            if (userGoal === 'increase') {
-                msg.style.color = 'rgb(255, 0, 0)'
-            } else {
-                msg.style.color = 'rgb(0, 255, 0)'
-            }
-            failureDisplay(msg)
-        }
+      msg.style.color = 'rgb(0, 0, 0)'
     }
+    successDisplay(msg)
+  } else {
+    if (progress < 0) {
+      let msg = document.getElementById('msgDecrease')
+      if (userGoal === 'increase') {
+        msg.style.color = 'rgb(0, 0, 0)'
+      } else {
+        msg.style.color = 'rgb(0, 0, 0)'
+      }
+      failureDisplay(msg)
+    }
+  }
 } else {
-    if (userGoal === 'decrease') {
-        if (progress > 0) {
-            let msg = document.getElementById('msgIncrease')
-            if (userGoal === 'increase') {
-                msg.style.color = 'rgb(0, 255, 0)'
-            } else {
-                msg.style.color = 'rgb(255, 0, 0)'
-            }
-            failureDisplay(msg);
+  if (userGoal === 'decrease') {
+    if (progress > 0) {
+      let msg = document.getElementById('msgIncrease')
+      if (userGoal === 'increase') {
+        msg.style.color = 'rgb(0, 0, 0)'
+      } else {
+        msg.style.color = 'rgb(0, 0, 0)'
+      }
+      failureDisplay(msg)
+    } else {
+      if (progress < 0) {
+        let msg = document.getElementById('msgDecrease')
+        if (userGoal === 'increase') {
+          msg.style.color = 'rgb(0, 0, 0)'
         } else {
-            if (progress < 0) {
-                let msg = document.getElementById('msgDecrease')
-                if (userGoal === 'increase') {
-                    msg.style.color = 'rgb(255, 0, 0)'
-                } else {
-                    msg.style.color = 'rgb(0, 255, 0)'
-                }
-                successDisplay(msg)
-            }
+          msg.style.color = 'rgb(0, 0, 0)'
         }
+        successDisplay(msg)
+      }
     }
+  }
 }
 
 function successDisplay(msg) {
-    let positiveProgress = progress
-    if (positiveProgress < 0) {
-        positiveProgress *= -1
-    }
-    msg.style.display = ''
-    let currentResult = document.getElementById('currentResult')
-    currentResult.style.color = 'rgb(0, 255, 0)'
-    currentResult.innerHTML = positiveProgress + ' кг'
-    success.style.display = ''
+  let positiveProgress = progress
+  if (positiveProgress < 0) {
+    positiveProgress *= -1
+  }
+  msg.style.display = ''
+  let currentResult = document.getElementById('currentResult')
+  currentResult.style.color = 'rgb(0, 0, 0)'
+  currentResult.innerHTML = positiveProgress + ' кг'
+  success.style.display = ''
 }
 
 function failureDisplay(msg) {
-    let positiveProgress = progress
-    if (positiveProgress < 0) {
-        positiveProgress *= -1
-    }
-    msg.style.display = ''
-    let currentResult = document.getElementById('currentResult')
-    currentResult.style.color = 'rgb(255, 0, 0)'
-    currentResult.innerHTML = positiveProgress + ' кг'
-    failure.style.display = ''
+  let positiveProgress = progress
+  if (positiveProgress < 0) {
+    positiveProgress *= -1
+  }
+  msg.style.display = ''
+  let currentResult = document.getElementById('currentResult')
+  currentResult.style.color = 'rgb(0, 0, 0)'
+  currentResult.innerHTML = positiveProgress + ' кг'
+  failure.style.display = ''
 }
